e6419580f5a8c4a13524ed23394060633ded894e
MartinKarapetyan <62433279+MartinKarapetyan@users.noreply.github.com>
2020-03-20 18:13:37 +0400

    Add files via upload

---
+++ javaSkript.js
@@ -0,0 +1,10 @@
+for (i = 30; i >= 0; i--) {
+            for (j = 0; j < i; j++) {
+             document.write("&nbsp&nbsp");
+           }
+           for (j = i; j <= 40-i; j++) {
+        document.write("9");
+    } 
+  document.write("<br>")
+  document.write("&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp");
+}     
