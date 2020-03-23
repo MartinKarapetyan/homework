a2aded293453a0f09ef104c7c032b8369143bbdb
MartinKarapetyan <62433279+MartinKarapetyan@users.noreply.github.com>
2020-03-23 13:32:23 +0400

    Add files via upload

---
+++ 3 tver nvazman/js.js
@@ -0,0 +1,26 @@
+let a = +prompt('arajin tiv');
+let b = +prompt('erkrord tiv');
+let c = +prompt('erord tiv');
+ 
+   if (a > b && a >c && b > c) {
+   	console.log('a,b,c');
+   }
+   else if (a > b && a >c && c > b) {
+   	console.log('a,c,b');
+   }
+  else if (b > a && b >c && a > c) {
+   	console.log('b,a,c');
+   }
+  else if (b > c && b >a && c > a) {
+   	console.log('b,c,a');
+   }
+  else if (c > a && c >b && a > b) {
+   	console.log('c,a,b');
+   }
+   else if (c > b && c >a && b > a) {
+   	console.log('c,b,a');
+   }
+   else{
+   	console.log(Sxal)
+   }
+    
