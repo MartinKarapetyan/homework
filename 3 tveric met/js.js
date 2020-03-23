5cf85bff8470462744de27abb93f78aa0d06d7f7
MartinKarapetyan <62433279+MartinKarapetyan@users.noreply.github.com>
2020-03-23 12:37:48 +0400

    Add files via upload

---
+++ 3 tveric met/js.js
@@ -0,0 +1,15 @@
+let a = +prompt('arajin tiv');
+let b = +prompt('erkrord tiv');
+let c = +prompt('erord tiv');
+ 
+   if (a > b && a >c) {
+   	alert('a meta');
+   }
+   else if (b > a && b > c) {
+   	alert('b meta');
+   }
+   else   {
+   	alert("c meta"); 
+   }
+
+    
